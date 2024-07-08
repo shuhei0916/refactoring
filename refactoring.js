34adad31059cc9249f5093bf6804543720f84908
shuhei0916 <shuheiito.stc@gmail.com>
2024-07-08 23:23:48 +0900

    added volumeCreditsFor function

--- refactoring.js
+++ refactoring.js
@@ -53,6 +53,13 @@ function amountFor(aPerformance) {
   return result;
 }
 
+function volumeCreditsFor(perf) {
+  let volumeCredits = 0;
+  volumeCredits += Math.max(perf.audience - 30, 0);
+  if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+  return volumeCredits;
+}
+
 function statement (invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
@@ -62,14 +69,9 @@ function statement (invoice, plays) {
                           { style: "currency", currency: "USD",
                             minimumFractionDigits: 2 }).format;
   
-    for (let perf of invoice.performances) {
-      let thisAmount = amountFor(perf);
-  
-      // add volume credits
-      volumeCredits += Math.max(perf.audience - 30, 0);
-      // add extra credit for every ten comedy attendees
-      if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
-  
+    for (let perf of invoice.performances) {  
+      volumeCredits += volumeCreditsFor(perf);
+
       // print line for this order
       result += `  ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
       totalAmount += amountFor(perf);
