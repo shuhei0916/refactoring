d65c5cbe0a0ace76fa2c0efe88ad31efd33db22b
shuhei0916 <shuheiito.stc@gmail.com>
2024-07-09 13:11:32 +0900

    removed volumeCredits and moved all functions inside statement function

--- refactoring.js
+++ refactoring.js
@@ -26,62 +26,69 @@ const plays = {
     }
   ];
 
-function playFor(aPerformance) {
-  return plays[aPerformance.playID];
-}
 
-// 関数の戻り値の変数はresultとする
-function amountFor(aPerformance) {
-  let result = 0;
-  switch (playFor(aPerformance).type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default: // switch文の中でほかの全てのcase条件に一致しなかった場合に実行されるセクション
-        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
-    }
-  return result;
-}
-
-function volumeCreditsFor(aPerformance) {
-  let result = 0;
-  result += Math.max(aPerformance.audience - 30, 0);
-  if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
-  return result;
-}
-
-function usd(aNumber) {
-  return new Intl.NumberFormat("en-US",
-    { style: "currency", currency: "USD",
-      minimumFractionDigits: 2 }).format(aNumber);
-}
 
 function statement (invoice, plays) {
     let totalAmount = 0;
-    let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
     // Intl.NumberFormatはjsの組み込みオブジェクトで、数値を言語に応じて適切な形式に変換するために使用される。
-  
+    
     for (let perf of invoice.performances) {  
-      volumeCredits += volumeCreditsFor(perf);
-
       // 注文の内訳を出力
       result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
       totalAmount += amountFor(perf);
     }
     result += `Amount owed is ${usd(totalAmount)}\n`;
-    result += `You earned ${volumeCredits} credits\n`;
+    result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
+
+    function playFor(aPerformance) {
+      return plays[aPerformance.playID];
+    }
+    
+    // 関数の戻り値の変数はresultとする
+    function amountFor(aPerformance) {
+      let result = 0;
+      switch (playFor(aPerformance).type) {
+        case "tragedy":
+          result = 40000;
+          if (aPerformance.audience > 30) {
+            result += 1000 * (aPerformance.audience - 30);
+          }
+          break;
+        case "comedy":
+          result = 30000;
+          if (aPerformance.audience > 20) {
+            result += 10000 + 500 * (aPerformance.audience - 20);
+          }
+          result += 300 * aPerformance.audience;
+          break;
+        default: // switch文の中でほかの全てのcase条件に一致しなかった場合に実行されるセクション
+            throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+        }
+      return result;
+    }
+    
+    function volumeCreditsFor(aPerformance) {
+      let result = 0;
+      result += Math.max(aPerformance.audience - 30, 0);
+      if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+      return result;
+    }
+    
+    function usd(aNumber) {
+      return new Intl.NumberFormat("en-US",
+        { style: "currency", currency: "USD",
+          minimumFractionDigits: 2 }).format(aNumber);
+    }
+    
+    function totalVolumeCredits() {
+      let volumeCredits = 0;
+      for (let perf of invoice.performances) {
+        volumeCredits += volumeCreditsFor(perf);
+      }
+      return volumeCredits;
+    }
 }
 
 console.log("hehe");
