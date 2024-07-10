6930b0f1aa2c0afcedd3ef7c7c56ca88c16c4dcd
shuhei0916 <shuheiito.stc@gmail.com>
2024-07-11 08:36:55 +0900

    moved statement() -> renderPlainText()

--- refactoring.js
+++ refactoring.js
@@ -1,97 +1,105 @@
 // plays.json データ
 const plays = {
-    "hamlet": {"name": "Hamlet", "type": "tragedy"},
-    "as-like": {"name": "As You Like It", "type": "comedy"},
-    "othello": {"name": "Othello", "type": "tragedy"}
-  };
-  
-  // invoices.json データ
-  const invoices = [
-    {
-      "customer": "BigCo",
-      "performances": [
-        {
-          "playID": "hamlet",
-          "audience": 55
-        },
-        {
-          "playID": "as-like",
-          "audience": 35
-        },
-        {
-          "playID": "othello",
-          "audience": 40
-        }
-      ]
-    }
-  ];
+  "hamlet": { "name": "Hamlet", "type": "tragedy" },
+  "as-like": { "name": "As You Like It", "type": "comedy" },
+  "othello": { "name": "Othello", "type": "tragedy" }
+};
 
-function statement (invoice, plays) {
-    let result = `Statement for ${invoice.customer}\n`;
-    for (let perf of invoice.performances) {  
-      // 注文の内訳を出力
-      result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+// invoices.json データ
+const invoices = [
+  {
+    "customer": "BigCo",
+    "performances": [
+      {
+        "playID": "hamlet",
+        "audience": 55
+      },
+      {
+        "playID": "as-like",
+        "audience": 35
+      },
+      {
+        "playID": "othello",
+        "audience": 40
+      }
+    ]
+  }
+];
+
+function statement(invoice, plays) {
+  return renderPlainText(invoice, plays);
+}
+
+
+function renderPlainText(invoice, plays) {
+
+  let result = `Statement for ${invoice.customer}\n`;
+  for (let perf of invoice.performances) {
+    // 注文の内訳を出力
+    result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+  }
+  result += `Amount owed is ${usd(totalAmount())}\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
+  return result;
+
+  function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
     }
-    result += `Amount owed is ${usd(totalAmount())}\n`;
-    result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
+  }
 
-    function totalAmount() {
-      let result = 0;
-      for (let perf of invoice.performances) {
-        result += amountFor(perf);
-      }
-      return result;
-    }
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
 
-    function playFor(aPerformance) {
-      return plays[aPerformance.playID];
-    }
-    
-    // 関数の戻り値の変数はresultとする
-    function amountFor(aPerformance) {
-      let result = 0;
-      switch (playFor(aPerformance).type) {
-        case "tragedy":
-          result = 40000;
-          if (aPerformance.audience > 30) {
-            result += 1000 * (aPerformance.audience - 30);
-          }
-          break;
-        case "comedy":
-          result = 30000;
-          if (aPerformance.audience > 20) {
-            result += 10000 + 500 * (aPerformance.audience - 20);
-          }
-          result += 300 * aPerformance.audience;
-          break;
-        default: // switch文の中でほかの全てのcase条件に一致しなかった場合に実行されるセクション
-            throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+  // 関数の戻り値の変数はresultとする
+  function amountFor(aPerformance) {
+    let result = 0;
+    switch (playFor(aPerformance).type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
         }
-      return result;
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default: // switch文の中でほかの全てのcase条件に一致しなかった場合に実行されるセクション
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
     }
-    
-    function volumeCreditsFor(aPerformance) {
-      let result = 0;
-      result += Math.max(aPerformance.audience - 30, 0);
-      if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
-      return result;
+    return result;
+  }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
+
+  function usd(aNumber) {
+    // Intl.NumberFormatはjsの組み込みオブジェクトで、数値を言語に応じて適切な形式に変換するために使用される。
+    return new Intl.NumberFormat("en-US",
+      {
+        style: "currency", currency: "USD",
+        minimumFractionDigits: 2
+      }).format(aNumber);
+  }
+
+  function totalVolumeCredits() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += volumeCreditsFor(perf);
     }
-    
-    function usd(aNumber) {
-      // Intl.NumberFormatはjsの組み込みオブジェクトで、数値を言語に応じて適切な形式に変換するために使用される。
-      return new Intl.NumberFormat("en-US",
-        { style: "currency", currency: "USD",
-          minimumFractionDigits: 2 }).format(aNumber);
-    }
-    
-    function totalVolumeCredits() {
-      let result = 0;
-      for (let perf of invoice.performances) {
-        result += volumeCreditsFor(perf);
-      }
-      return result;
-    }
+    return result;
+  }
 }
 
 console.log("hehe");
