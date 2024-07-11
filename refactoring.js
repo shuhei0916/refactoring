c94f81341dc15e6001b9ce8e3084441bccbd3d41
shuhei0916 <shuheiito.stc@gmail.com>
2024-07-11 09:15:01 +0900

    added playFor() in statement()

--- refactoring.js
+++ refactoring.js
@@ -35,7 +35,12 @@ function statement(invoice, plays) {
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
+    result.play = playFor(result);
     return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.plaID];
   }
 }
 
