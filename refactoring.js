df8e63bd0180c81cacfc553fbb8d37d7a557c92e
shuhei0916 <shuheiito.stc@gmail.com>
2024-07-11 09:07:42 +0900

    added enrichPerformance() in statement

--- refactoring.js
+++ refactoring.js
@@ -29,8 +29,14 @@ const invoices = [
 function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances;
+  statementData.performances = invoice.performances.map(enrichPerformance);
+  console.log("hoge: ", statementData);
   return renderPlainText(statementData, plays);
+
+  function enrichPerformance(aPerformance) {
+    const result = Object.assign({}, aPerformance);
+    return result;
+  }
 }
 
 
