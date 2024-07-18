8352e0a60bca454a398684c20b15d6a51e59b49b
shuhei0916 <shuheiito.stc@gmail.com>
2024-07-19 08:44:08 +0900

    added Mocha and test.js

---
+++ vite-project/test.js
@@ -0,0 +1,9 @@
+import assert from 'assert';
+
+describe('Array', () => {
+  describe('#indexOf()', () => {
+    it('指定された値が見つからない場合は -1 を返します。', () => {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
