bc38a833fa5cb8d0325dfac51fb6dea1bcc72299
shuhei0916 <shuheiito.stc@gmail.com>
2024-07-20 23:53:55 +0900

    added new test in test.js

--- test.js
+++ test.js
@@ -13,4 +13,9 @@ describe('province', function() {
   it('profit', function() {
     assert.equal(asia.profit, 230);
   });
+  it('change production', function() {
+    asia.producers[0].production = 20;
+    assert.equal(asia.shortfall, -6);
+    assert.equal(asia.profit, 292);
+  })
 });
