8490846b0e99e8aeda9560dd2abe7077525f4e61
shuhei0916 <shuheiito.stc@gmail.com>
2024-07-20 23:43:20 +0900

    added another test in test.js

--- test.js
+++ test.js
@@ -1,9 +1,15 @@
 import assert from 'assert';
 import {Province, sampleProvinceData} from './province.js';
+import { hasUncaughtExceptionCaptureCallback } from 'process';
 
 describe('province', function() {
   it('shortfall', function() {
     const asia = new Province(sampleProvinceData());
     assert.equal(asia.shortfall, 5);
+  })
+  it('profit', function() {
+    const asia = new Province(sampleProvinceData());
+    // assert.equal(actual, expected);
+    assert.equal(asia.profit, 230);
   })
 })
